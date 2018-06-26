9d295ad5ed737faf02d97406e65667f9cc1a1fba
zsly3n3 <zsly3n@163.com>
2018-06-27 00:43:51 +0800

    add

--- src/components/vue-accordion.vue
+++ src/components/vue-accordion.vue
@@ -1,68 +1,46 @@
 <template>
-<div :class="accordionClass" :style="divStyles">
-<ul v-on:mouseleave="mouseOut()">
-<li v-for="(item,index) in items" v-on:mouseover="mouseOver(index)" :id="index" :item="item" :key="index" :style="{backgroundImage: 'url(' + item.image + ')'}">
+<div class="vue-accordion">
+<ul v-on:mouseleave="mouseLeave">
+<li v-for="(item,index) in items" v-on:mouseenter="mouseOver(index)" :id="index" :item="item" :key="index" :style="{backgroundImage: 'url(' + item.image + ')'}">
 </li>
 </ul>
 </div>
 </template>
 <script>
-// <partial-accordion v-for="(item,index) in items" :item="item" :key="index" :styles="styles"></partial-accordion>
-// import partialAccordion from './vue-accordion-partial.vue'
 export default {
   name: 'vue-accordion',
   props: {
     items: {
       type: Array
     },
-    styles: {
-      type: Object,
-      default: function () {
-        return {}
-      }
-    },
-    accordionClass: {
-      type: String,
-      default: 'vue-accordion'
-    }
+    lastTimer: null
   },
   methods: {
     mouseOver: function (index) {
       var clientWidth = document.documentElement.clientWidth
       if (clientWidth < 1000) {
-        clientWidth = 1000
+        clientWidth = 1000.0
       }
-      let count = 7.0
+      let count = this.items.length
       var per = clientWidth / count
       var currentWidth = this.items[index].width
       var x = (clientWidth - currentWidth) / (count - 1.0)
-      var otherRs = x / per * 100 + '%'
-      var rs = currentWidth / per * 100 + '%'
+      var otherRs = x / per * 100.0 + '%'
+      var rs = currentWidth / per * 100.0 + '%'
+      var current = document.getElementById(index)
+      current.style.width = rs
       for (var i = 0; i < count; i++) {
         var obj = document.getElementById(i)
-        if (i === index) {
-          obj.style.width = rs
-        } else {
+        if (i !== index) {
           obj.style.width = otherRs
         }
       }
     },
-    mouseOut: function () {
-      let count = 7.0
-      var clientWidth = document.documentElement.clientWidth
-      if (clientWidth < 1000) {
-        clientWidth = 1000
-      }
-      var per = clientWidth / count * 100 + '%'
-      for (var i = 0; i < count; i++) {
+    mouseLeave: function () {
+      for (var i = 0; i < this.items.length; i++) {
         var obj = document.getElementById(i)
-        obj.style.width = per
+        obj.style.width = 100 + '%'
       }
-    }
-  },
-  computed: {
-    divStyles () {
-      return this.styles && this.styles.div ? this.styles.div : {}
     }
   }
 }
@@ -72,7 +50,7 @@ export default {
 width: 100%;
 overflow: hidden;
 height: 600px;
-min-width: 1200px;
+min-width: 1000px;
 }
 .vue-accordion ul {
 width: 100%;
@@ -89,9 +67,11 @@ position: relative;
 width: 100%;
 background-repeat: no-repeat;
 background-position: center center;
-transition: all 500ms ease;
 height: 100%;
+transition: all 0.5s ease;
+-o-transition: all 0.5s ease; /* opera */
+-ms-transition: all 0.5s ease; /* IE 10 */
+-moz-transition: all 0.5s ease; /* Firefox */
+-webkit-transition: all 0.5s ease; /*safari and chrome */
 }
-/* .vue-accordion ul:hover li { width: 57.7%; } */
-/* .vue-accordion ul:hover li:hover { width: 353.74%; } */
 </style>
