fe7810322792932e5813491a92b5489a23e56e9f
王孟佳 <wangmengjia@wangmengjiadeMac-mini.local>
2018-06-26 19:56:38 +0800

    add

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+  <div class="d1">
+  </div>
+  <vue-accordion :items="items">
+  </vue-accordion>
+</div>
+</template>
+
+<script>
+import vueAccordion from './vue-accordion.vue'
+export default {
+  name: 'HelloWorld',
+  components: {
+    vueAccordion
+  },
+  data () {
+    return {
+      items: [
+        {image: '/static/1.png', width: 897},
+        {image: '/static/2.jpg', width: 450},
+        {image: '/static/3.jpg', width: 800},
+        {image: '/static/4.jpg', width: 800},
+        {image: '/static/5.jpg', width: 800},
+        {image: '/static/6.jpg', width: 800},
+        {image: '/static/7.jpg', width: 874}
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.d1{
+  width: 100%;
+  height: 400px;
+  background-color: orangered;
+}
+/* .vue-accordion ul:hover li { width: 2%; } */
+</style>
---
+++ src/components/vue-accordion-partial.vue
@@ -0,0 +1,65 @@
+<template>
+<li :style="[liStyles,wdStyle]" v-on:mouseover="mouseOver($event)">
+</li>
+</template>
+<script>
+export default {
+  props: {
+    item: {
+      type: Object
+    },
+    styles: {
+      type: Object
+    }
+  },
+  data () {
+    return {
+      wdStyle: {
+        // width: 100 + '%'
+      }
+    }
+  },
+  methods: {
+    mouseOver: function (event) {
+      var clientWidth = document.documentElement.clientWidth
+      if (clientWidth < 1000) {
+        clientWidth = 1000
+      }
+      var per = clientWidth / 7.0
+      var currentWidth = this.item.width
+      var x = (clientWidth - currentWidth) / 6.0
+      var otherRs = x / per * 100 + '%'
+      var rs = currentWidth / per * 100 + '%'
+      // this.wdStyle = {
+      //   width: rs
+      // }
+      console.log('clientWidth:' + clientWidth + ' ,otherRs:' + otherRs + ', rs:' + rs)
+    }
+  },
+  computed: {
+    liStyles () {
+      const li = {
+        backgroundImage: 'url(' + this.item.image + ')'
+      }
+      if (this.styles && this.styles.li) {
+        Object.assign(li, this.styles.li)
+      }
+      return li
+    }
+  }
+}
+</script>
+<style>
+.vue-accordion ul li {
+display: table-cell;
+vertical-align: bottom;
+position: relative;
+width: 100%;
+background-repeat: no-repeat;
+background-position: center center;
+transition: all 500ms ease;
+height: 100%;
+}
+.vue-accordion ul:hover li { width: 57.7%; }
+.vue-accordion ul:hover li:hover { width: 353.74%; }
+</style>
